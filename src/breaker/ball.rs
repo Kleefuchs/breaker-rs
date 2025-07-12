d4f9b06af3d63052dde1b44467c293b6fa341ad6
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-07-12 16:02:42 +0200

    Saved some changes to ball.rs

--- src/breaker/ball.rs
+++ src/breaker/ball.rs
@@ -1,6 +1,17 @@
 use raylib::prelude::*;
 
+use crate::gameobject;
+
 pub struct Ball {
     radius: f32,
     pos: Vector2
+}
+
+impl gameobject::Gameobject for Ball {
+    fn update(&mut self) {
+    }
+
+    fn draw<T>(&self, texture_mode: &mut RaylibTextureMode<T>) {
+
+    }
 }
