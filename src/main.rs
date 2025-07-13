e921ddc6957f2b567764942fbedaa826a29d9b4b
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2025-07-13 11:20:31 +0200

    Added a main "menu"

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,213 @@
-
 use raylib::prelude::*;
 
-use crate::{gameobject::Gameobject, controllable::Controllable, page::Page};
+use crate::{controllable::Controllable, gameobject::Gameobject, page::Page};
 
 mod breaker;
-mod gameobject;
-mod rl;
 mod controllable;
-mod page;
-mod gamestate;
 mod game_over;
-mod text_data;
-mod pause;
+mod gameobject;
+mod gamestate;
 mod menu;
+mod page;
+mod pause;
+mod rl;
+mod text_data;
 
 fn create_default_breaker(world_size: Vector2) -> breaker::Breaker {
-    breaker::Breaker::new(world_size.x as i32, world_size.y as i32, 2.5, &mut [Color::RED, Color::ORANGE, Color::YELLOW, Color::GREEN, Color::BLUE, Color::PURPLE])
+    breaker::Breaker::new(
+        world_size.x as i32,
+        world_size.y as i32,
+        2.5,
+        &mut [
+        Color::RED,
+        Color::ORANGE,
+        Color::YELLOW,
+        Color::GREEN,
+        Color::BLUE,
+        Color::PURPLE,
+        ],
+    )
 }
 
-fn create_text_in_middle_of_screen<'a, Tfont: RaylibFont>(text: &'a str, world_size: Vector2, font: &'a Tfont, font_size: f32, measured_text: i32) -> text_data::TextData<'a, Tfont> {
-        text_data::TextData {
-            font,
-            text,
-            position: Vector2 {x: world_size.x / 2.0, y: world_size.y / 3.0},
-            origin: Vector2 {x: measured_text as f32 / 2.0, y: 0.0},
-            rotation: 0.0,
-            font_size,
-            spacing: 1.0,
-            tint: Color::WHITE,
-        }
+fn create_text_in_middle_of_screen<'a, Tfont: RaylibFont>(
+    text: &'a str,
+    world_size: Vector2,
+    font: &'a Tfont,
+    font_size: f32,
+    offset_x: i32,
+) -> text_data::TextData<'a, Tfont> {
+    text_data::TextData {
+        font,
+        text,
+        position: Vector2 {
+            x: world_size.x / 2.0,
+            y: world_size.y / 3.0,
+        },
+        origin: Vector2 {
+            x: offset_x as f32 / 2.0,
+            y: 0.0,
+        },
+        rotation: 0.0,
+        font_size,
+        spacing: 1.0,
+        tint: Color::WHITE,
+    }
 }
 
 fn main() {
-    let world_size: Vector2 = Vector2 {x: 640.0, y: 360.0};
+    let world_size: Vector2 = Vector2 { x: 640.0, y: 360.0 };
     let mut rl = rl::Rl::from(raylib::init().size(1280, 720).title("Breaker").build());
-    let mut render_texture: RenderTexture2D = rl.handle.load_render_texture(&rl.thread, world_size.x as u32, world_size.y as u32).unwrap();
+    let mut render_texture: RenderTexture2D = rl
+        .handle
+        .load_render_texture(&rl.thread, world_size.x as u32, world_size.y as u32)
+        .unwrap();
     let mut breaker: breaker::Breaker = create_default_breaker(world_size);
     let font_default = rl.handle.get_font_default();
-    let mut game_over: game_over::GameOver<WeakFont> = game_over::GameOver::new(create_text_in_middle_of_screen("Game Over", world_size, &font_default, 30.0, rl.handle.measure_text("Game Over", 30)));
-    let mut pause: pause::Pause<WeakFont> = pause::Pause::new(create_text_in_middle_of_screen("Game Paused", world_size, &font_default, 30.0, rl.handle.measure_text("Game Paused", 30)));
-    let mut gamestate: gamestate::Gamestate = gamestate::Gamestate::Running;
+    let mut game_over: game_over::GameOver<WeakFont> =
+        game_over::GameOver::new(create_text_in_middle_of_screen(
+                "Game Over",
+                world_size,
+                &font_default,
+                30.0,
+                rl.handle.measure_text("Game Over", 30),
+        ));
+    let mut pause: pause::Pause<WeakFont> = pause::Pause::new(create_text_in_middle_of_screen(
+            "Game Paused",
+            world_size,
+            &font_default,
+            30.0,
+            rl.handle.measure_text("Game Paused", 30),
+    ));
 
+    let mut menu: menu::Menu<WeakFont> = menu::Menu::new(text_data::TextData {
+        font: &font_default,
+        text: "Breaker-rs: Breakout written in Rust!",
+        position: Vector2 {x: world_size.x / 2.0, y: world_size.y / 4.0},
+        origin: Vector2 {x: rl.handle.measure_text("Breaker-rs: Breakout written in Rust!", 30) as f32 / 2.0, y: 0.0},
+        rotation: 0.0,
+        font_size: 30.0,
+        spacing: 3.0,
+        tint: Color::WHITE,
+    });
 
+    let mut gamestate: gamestate::Gamestate = gamestate::Gamestate::Menu;
 
     while !rl.handle.window_should_close() {
         match gamestate {
             gamestate::Gamestate::Running => {
-                breaker.control(&rl.handle, &mut [KeyboardKey::KEY_A, KeyboardKey::KEY_D, KeyboardKey::KEY_SPACE]);
+                breaker.control(
+                    &rl.handle,
+                    &mut [
+                    KeyboardKey::KEY_A,
+                    KeyboardKey::KEY_D,
+                    KeyboardKey::KEY_SPACE,
+                    ],
+                );
                 breaker.update(&rl.handle);
                 gamestate = breaker.get_current_state();
                 {
-                    let mut texture_mode = rl.handle.begin_texture_mode(&rl.thread, &mut render_texture);
+                    let mut texture_mode = rl
+                        .handle
+                        .begin_texture_mode(&rl.thread, &mut render_texture);
                     breaker.draw(&mut texture_mode);
                 }
-            },
+            }
             gamestate::Gamestate::Paused => {
                 pause.should_unpause = false;
                 pause.control(&rl.handle, &mut [KeyboardKey::KEY_SPACE]);
-                let mut texture_mode = rl.handle.begin_texture_mode(&rl.thread, &mut render_texture);
+                let mut texture_mode = rl
+                    .handle
+                    .begin_texture_mode(&rl.thread, &mut render_texture);
                 breaker.draw(&mut texture_mode);
                 pause.draw(&mut texture_mode);
                 gamestate = pause.get_current_state();
-            },
+            }
 
             gamestate::Gamestate::Resume => {
                 pause.should_unpause = true;
                 breaker.should_pause = false;
-                let mut texture_mode = rl.handle.begin_texture_mode(&rl.thread, &mut render_texture);
+                menu.should_start = false;
+                let mut texture_mode = rl
+                    .handle
+                    .begin_texture_mode(&rl.thread, &mut render_texture);
                 breaker.draw(&mut texture_mode);
                 gamestate = gamestate::Gamestate::Running;
-            },
+            }
 
             gamestate::Gamestate::GameOver => {
                 game_over.control(&rl.handle, &mut [KeyboardKey::KEY_SPACE]);
                 game_over.update(&rl.handle);
                 gamestate = game_over.get_current_state();
-                let mut texture_mode = rl.handle.begin_texture_mode(&rl.thread, &mut render_texture);
+                let mut texture_mode = rl
+                    .handle
+                    .begin_texture_mode(&rl.thread, &mut render_texture);
                 game_over.draw(&mut texture_mode);
-            },
+            }
 
             gamestate::Gamestate::Init => {
                 breaker = create_default_breaker(world_size);
                 gamestate = gamestate::Gamestate::Running;
-                game_over = game_over::GameOver::new(create_text_in_middle_of_screen("Game Over", world_size, &font_default, 30.0, rl.handle.measure_text("Game Over", 30)));
-                pause = pause::Pause::new(create_text_in_middle_of_screen("Game Paused", world_size, &font_default, 30.0, rl.handle.measure_text("Game Paused", 30)));
-            },
+                game_over = game_over::GameOver::new(create_text_in_middle_of_screen(
+                        "Game Over",
+                        world_size,
+                        &font_default,
+                        30.0,
+                        rl.handle.measure_text("Game Over", 30),
+                ));
 
-            gamestate::Gamestate::Menu => {
+                pause = pause::Pause::new(create_text_in_middle_of_screen(
+                        "Game Paused",
+                        world_size,
+                        &font_default,
+                        30.0,
+                        rl.handle.measure_text("Game Paused", 30),
+                ));
+
+                menu = menu::Menu::new(text_data::TextData {
+                    font: &font_default,
+                    text: "Breaker-rs: Breakout written in Rust!",
+                    position: Vector2 {x: world_size.x / 2.0, y: world_size.y / 4.0},
+                    origin: Vector2 {x: rl.handle.measure_text("Breaker-rs: Breakout written in Rust!", 30) as f32 / 2.0, y: 0.0},
+                    rotation: 0.0,
+                    font_size: 30.0,
+                    spacing: 3.0,
+                    tint: Color::WHITE,
+                });
 
             }
+
+            gamestate::Gamestate::Menu => {
+                menu.control(&rl.handle, &mut [KeyboardKey::KEY_SPACE]);
+                let mut texture_mode = rl
+                    .handle
+                    .begin_texture_mode(&rl.thread, &mut render_texture);
+                texture_mode.clear_background(Color::BLACK);
+                menu.draw(&mut texture_mode);
+
+                gamestate = menu.get_current_state();
+            }
         }
-                let screen_width: i32 = rl.handle.get_screen_width();
-                let screen_height: i32 = rl.handle.get_screen_height();
-                let mut draw_handle: RaylibDrawHandle = rl.handle.begin_drawing(&rl.thread);
-                draw_handle.clear_background(Color::BLACK);
-                draw_handle.draw_texture_pro(&render_texture, Rectangle {x: 0.0, y: 0.0, width: world_size.x, height: -world_size.y}, Rectangle {x: 0.0, y: 0.0, width: screen_width as f32, height: screen_height as f32}, Vector2 {x: 0.0, y: 0.0}, 0.0, Color::WHITE);
+        let screen_width: i32 = rl.handle.get_screen_width();
+        let screen_height: i32 = rl.handle.get_screen_height();
+        let mut draw_handle: RaylibDrawHandle = rl.handle.begin_drawing(&rl.thread);
+        draw_handle.clear_background(Color::BLACK);
+        draw_handle.draw_texture_pro(
+            &render_texture,
+            Rectangle {
+                x: 0.0,
+                y: 0.0,
+                width: world_size.x,
+                height: -world_size.y,
+            },
+            Rectangle {
+                x: 0.0,
+                y: 0.0,
+                width: screen_width as f32,
+                height: screen_height as f32,
+            },
+            Vector2 { x: 0.0, y: 0.0 },
+            0.0,
+            Color::WHITE,
+        );
     }
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,23 +1,47 @@
 use raylib::prelude::*;
 
-use crate::{gameobject, gamestate, page};
+use crate::{controllable, gameobject, gamestate, page, text_data};
 
 
 
-pub struct Menu {
+pub struct Menu<'a, Tfont1: RaylibFont> {
+    pub should_start: bool,
+    main_text: text_data::TextData<'a, Tfont1>
 }
 
-impl gameobject::Gameobject for Menu {
+impl<'a, Tfont1: RaylibFont> gameobject::Gameobject for Menu<'a, Tfont1> {
     fn update(&mut self, handle: &raylib::RaylibHandle) {
     }
 
     fn draw<T>(&self, texture_mode: &mut raylib::prelude::RaylibTextureMode<T>) {
         texture_mode.clear_background(Color::BLACK);
+        self.main_text.draw(texture_mode);
     }
 }
 
-impl page::Page for Menu {
+impl<'a, Tfont1: RaylibFont> controllable::Controllable for Menu<'a, Tfont1> {
+    fn control(&mut self, handle: &RaylibHandle, keys: &mut [KeyboardKey]) {
+        assert!(keys.len()==1);
+        if handle.is_key_pressed(keys[0]) {
+            self.should_start = true;
+        }
+    }
+}
+
+impl<'a, Tfont1: RaylibFont> page::Page for Menu<'a, Tfont1> {
     fn get_current_state(&self) -> crate::gamestate::Gamestate {
+        if self.should_start {
+            return gamestate::Gamestate::Init;
+        }
         gamestate::Gamestate::Menu
+    }
+}
+
+impl<'a, Tfont1: RaylibFont> Menu<'a, Tfont1> {
+    pub fn new(main_text: text_data::TextData<'a, Tfont1>) -> Menu<'a, Tfont1> {
+        Menu {
+            should_start: false,
+            main_text
+        }
     }
 }
